310adf26f38b1b7e0afd837209b8593d65e8d4c2
khanh <khanhnguyen1123@gmail.com>
2016-09-20 11:26:56 -0700

    firstInitialization

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser= require('body-parser');
+
+
+var app = express();
+
+//mongoose.connect('mongodb://localhost:27017/time-waste')
+mongoose.connect('mongodb://khanh:khanh@ds033056.mlab.com:33056/time-waste');
+app.use(bodyParser.json());
+//app.use(multipartMiddleware);
+app.use('/public',express.static(__dirname + "/public"));
+app.use('/node_modules',express.static(__dirname+'/node_modules'));
+
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname+'/index.html');
+});
+
+
+var port = process.env.PORT  || 5000;
+app.listen(port, function(){
+	console.log("SE 195 Successfull connected to mongodb server (local host: )"+port);
+});

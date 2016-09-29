e79b183c3ccf693c2242f0a3b89f8a836e9f9371
khanh <khanhnguyen1123@gmail.com>
2016-09-29 11:41:29 -0700

    addingBackendForuserpassport

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser= require('body-parser');
+var passport = require('passport');
+var jwt = require('express-jwt');
 
+var auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload'
+});
+
+reuqire('./server/controllers/passport');
 
 var app = express();
-// create a collection named time-waste in your local mongodb server to run this line of code
+var authenticationController = require('./server/controllers/authentication');
+
+//  [khanh] create a collection named time-waste in your local mongodb server to run this line of code
 mongoose.connect('mongodb://localhost:27017/time-waste')
 
 app.use(bodyParser.json());
 //app.use(multipartMiddleware);
 app.use('/public',express.static(__dirname + "/public"));
 app.use('/node_modules',express.static(__dirname+'/node_modules'));
+app.use(passport.initialize());
 
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname+'/index.html');
 });
 
+
+// authentication
+app.post('/api/user/register', authenticationController.register);
+app.post('/api/user/login', authenticationController.login);
+
+// profile
+app.get('/api/profile', auth, authenticationController.profileRead);
 
 var port = process.env.PORT  || 5000;
 app.listen(port, function(){
---
+++ server/controllers/profile.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+module.exports.profileRead = function(req, res) {
+
+  console.log("khanh test inside profile "+req.payload.email+"  khanh "+req.payload._id+ " khanh "+req.MY_SECRET);
+
+  if (!req.payload._id) {
+    res.status(401).json({
+      "message" : "UnauthorizedError: private profile"
+    });
+  } else {
+    User
+      .findById(req.payload._id)
+      .exec(function(err, user) {
+        res.status(200).json(user);
+      });
+  }
+
+};

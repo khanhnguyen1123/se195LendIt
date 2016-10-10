eab113c5f85ea6b06d2b4a9a5e0136f617cf47be
Duc Nguyen <ductrungnguyen92@gmail.com>
2016-10-10 14:41:34 -0700

    revert

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser= require('body-parser');
-
-
-var app = express();
-// create a collection named time-waste in your local mongodb server to run this line of code
-mongoose.connect('mongodb://localhost:27017/time-waste')
-
-app.use(bodyParser.json());
-//app.use(multipartMiddleware);
-app.use('/public',express.static(__dirname + "/public"));
-app.use('/node_modules',express.static(__dirname+'/node_modules'));
-
-
-app.get('/',function(req,res){
-	res.sendFile(__dirname+'/index.html');
-});
-
-
-var port = process.env.PORT  || 5000;
-app.listen(port, function(){
-	console.log("SE 195 Successfull connected to mongodb server (local host: )"+port);
-=======
 /************************************************************************
 * REQUIRE MODULES
 ************************************************************************/
@@ -82,5 +57,4 @@ app.listen(config.port,function (err){
     	console.log("Listening on port 3000");
     }
 
->>>>>>> d9bfbcba048b0c491c4f773330fbbc250bd2ccdd
 });
